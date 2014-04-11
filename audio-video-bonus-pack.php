33cb0858388be37ca5baf3937e2ea7cc2054417e
Scott Taylor <scott.c.taylor@mac.com>
2014-04-11 19:53:53 -0400

    More code

--- audio-video-bonus-pack.php
+++ audio-video-bonus-pack.php
@@ -25,6 +25,7 @@ class AudioVideoBonusPack {
 	}
 
 	private function __construct() {
+		add_action( 'init', array( $this, 'check_for_activity' ) );
 		add_action( 'admin_init', array( $this, 'admin_init' ) );
 
 		$this->ffmpeg_bin = get_option( 'ffmpeg_path' );
@@ -35,7 +36,87 @@ class AudioVideoBonusPack {
 			return;
 		}
 
+		if ( get_transient( 'av_encoding_media' ) ) {
+			add_action( 'admin_notices', array( $this, 'ffmpeg_encoding_notice' ) );
+		}
+
 		add_filter( 'wp_generate_attachment_metadata', array( $this, 'wp_generate_attachment_metadata' ), 10, 2 );
+	}
+
+	function check_for_activity() {
+		if ( isset( $_GET['action'] ) && 'encode' === $_GET['action'] ) {
+			error_log( 'wants to encode' );
+			$check = wp_verify_nonce( $_GET['_wpnonce'], 'encode' );
+			if ( ! $check ) {
+				error_log( 'nonce failed' );
+				//return;
+			}
+
+			$id = (int) $_GET['media_id'];
+			if ( empty( $id ) ) {
+				error_log( 'no id' );
+				return;
+			}
+
+			error_log( 'supposedly encoding' );
+			$this->encode_media( $id );
+		}
+	}
+
+	function encode_media( $id ) {
+		$file = get_attached_file( $id );
+		$ext = wp_check_filetype( $file );
+
+		$ffmpeg = $this->get_ffmpeg();
+		$video = $ffmpeg->open( $file );
+
+		$base = basename( $file );
+		$front = rtrim( $file, $base );
+
+		$attachment = get_post( $id, ARRAY_A );
+		$props = $attachment;
+		unset( $props['post_mime_type'], $props['guid'], $props['post_name'], $props['ID'] );
+		$thumbnail_id = get_post_thumbnail_id( $id );
+
+		foreach ( array( 'webm', 'ogv' ) as $type ) {
+			$key = '_' . $type . '_fallback';
+			$meta = get_post_meta( $id, $key, true );
+			if ( empty( $meta ) ) {
+				$type_file = $front . rtrim( $base, $ext['ext'] ) . $type;
+
+				try {
+					set_transient( 'av_encoding_media', true );
+
+					switch ( $type ) {
+					case 'ogv':
+						$video->save( new FFMpeg\Format\Video\Ogg(), $type_file );
+						break;
+					case 'webm':
+						$video->save( new FFMpeg\Format\Video\WebM(), $type_file );
+						break;
+					}
+				} catch ( Exception $e ) {
+					delete_transient( 'av_encoding_media' );
+
+					break;
+				}
+
+				$type_ext = wp_check_filetype( $type_file );
+				$props['post_mime_type'] = $type_ext['type'];
+
+				$attachment_id = wp_insert_attachment( $props, $type_file );
+				update_post_meta( $id, $key, $attachment_id );
+
+				$attach_data = wp_generate_attachment_metadata( $attachment_id, $type_file );
+				wp_update_attachment_metadata( $attachment_id, $attach_data );
+
+				if ( ! empty( $thumbnail_id ) ) {
+					set_post_thumbnail( $attachment_id, $thumbnail_id );
+				}
+			}
+
+			delete_transient( 'av_encoding_media' );
+		}
 	}
 
 	function wp_generate_attachment_metadata( $data, $post_id ) {
@@ -44,50 +125,14 @@ class AudioVideoBonusPack {
 
 		switch ( $ext['type'] ) {
 		case 'video/mp4':
-			$ffmpeg = $this->get_ffmpeg();
-			$video = $ffmpeg->open( $file );
+			$url = add_query_arg( array(
+				'action' => 'encode',
+				'media_id' => $post_id
+			), home_url( '/' ) );
 
-			$base = basename( $file );
-			$front = rtrim( $file, $base );
-
-			$attachment = get_post( $post_id, ARRAY_A );
-			$props = $attachment;
-			unset( $props['post_mime_type'], $props['guid'], $props['post_name'], $props['ID'] );
-			$thumbnail_id = get_post_thumbnail_id( $post_id );
-
-			foreach ( array( 'webm', 'ogv' ) as $type ) {
-				$key = '_' . $type . '_fallback';
-				$meta = get_post_meta( $post_id, $key, true );
-				if ( empty( $meta ) ) {
-					$type_file = $front . rtrim( $base, $ext['ext'] ) . $type;
-
-					//try {
-						switch ( $type ) {
-						case 'ogv':
-							$video->save( new FFMpeg\Format\Video\Ogg(), $type_file );
-							break;
-						case 'webm':
-							$video->save( new FFMpeg\Format\Video\WebM(), $type_file );
-							break;
-						}
-//					} catch ( Exception $e ) {
-//						break 2;
-//					}
-
-					$type_ext = wp_check_filetype( $type_file );
-					$props['post_mime_type'] = $type_ext['type'];
-
-					$attachment_id = wp_insert_attachment( $props, $type_file );
-					update_post_meta( $post_id, $key, $attachment_id );
-
-					$attach_data = wp_generate_attachment_metadata( $attachment_id, $type_file );
-					wp_update_attachment_metadata( $attachment_id, $attach_data );
-
-					if ( ! empty( $thumbnail_id ) ) {
-						set_post_thumbnail( $attachment_id, $thumbnail_id );
-					}
-				}
-			}
+			$nonced = html_entity_decode( wp_nonce_url( $url, 'encode' ) );
+			error_log( $nonced );
+			wp_remote_get( $nonced, array( 'blocking' => false ) );
 			break;
 		}
 
@@ -97,7 +142,8 @@ class AudioVideoBonusPack {
 	function get_ffmpeg() {
 		return FFMpeg\FFMpeg::create( array(
 			'ffmpeg.binaries' => $this->ffmpeg_bin,
-			'ffprobe.binaries' => $this->ffprobe_bin
+			'ffprobe.binaries' => $this->ffprobe_bin,
+			'ffmpeg.threads' => 60
 		) );
 	}
 
@@ -110,6 +156,12 @@ class AudioVideoBonusPack {
 <div class="error"><p><strong>Audio / Video Bonus Pack</strong> isn't very useful without <code>ffmpeg</code>.
 		<a href="<?php echo wp_nonce_url( 'plugins.php?action=deactivate&amp;plugin=' . $plugin_file, 'deactivate-plugin_' . $plugin_file );
 		?>">Uninstall the plugin</a> or <a href="<?php echo admin_url( 'options-media.php' ) ?>">update your settings</a>.</p></div>
+	<?php
+	}
+
+	function ffmpeg_encoding_notice() {
+	?>
+<div class="updated"><p><strong>Encoding media</strong> You have media that is being encoded....</p></div>
 	<?php
 	}
 
